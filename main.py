d3ed080f1e2387b6418c32185b9718621a332794
Bryan Delgado <bryand9970@gmail.com>
2023-10-30 18:31:46 -0500

    Main Structure for bot

--- main.py
+++ main.py
@@ -1,29 +1,73 @@
 import telebot
+import time
+import emoji
 import os
+from config import *
 
-#Telegram API key
-TOKEN = '6929440962:AAF8LpF4-d0HzVioDz2EReKSqMq1bmVzJHA'
+#Commands List
+commands = ["start", "matriculas", "praticas", "vinculacion", "preguntas_frecuentes", "comentarios", "contactos"]
 
-#Bot Connection
-bot = telebot.TeleBot(TOKEN)
+#Links and important data
+calendario_2023b = '<a href="https://esfot.epn.edu.ec/index.php/esfot/676-calendario-academico-2023-b">Calendario Académico 2023-B</a>' + '\n'
+guia_2023b = '<a href="https://esfot.epn.edu.ec/index.php/guia-del-estudiante">Guia del Estudiante 2023-B</a>' + '\n'
 
-#Bot Message List Commands
-@bot.message_handler(commands=["start", "matriculas", "help"])
+#Media
+start_img = open("./images/logo2.png", "rb")
 
-#Messages List Handler
-def start_msg(message):
-    bot.reply_to(message, 'Hola, me llamo Esfotito y seré tu asistente para conocer sobre los procesos académicos en la ESFOT.')
-    
-def matriculas_msg(message):
-    bot.reply_to(message, 'Las matriculas del periodo 2023-B ya han empezado. No te quedes sin tu cupo.')
-    
-#Bot Reponses for "no commands" lines
+#Docs
+guia_doc = open("./documents/gu_a_para_el_estudiante_2023-b.pdf", "rb")
+calendario_doc = open("./documents/calendario-academico-2023-b_actualizado.pdf", "rb")
+
+#Realases, Titles and Data
+important_title = '<b>💁 IMPORTANTE</b>'
+advise_title = '<b>⚠ AVISO</b>'
+#Major Titles
+calendario_title = '<b><u>🗓 Calendario Académico:</u></b>'
+guide_title = '<b><u>📑 Guía del Estudiante:</u></b>'
+vinculacion_title = '<b><u>👥 Prácticas Comunitarias (Vinculación):</u></b>'
+matricula_title = '<b><u>🎫 Guía de Matriculación:</u></b>'
+pasantias_title = '<b><u>🧑‍💼👨‍💼 Prácticas Pre-profesionales y Pasantías:</u></b>'
+
+#Bot Instances and Connection with API
+bot = telebot.TeleBot(Telegram_Token)
+
+#Actions and Responses for Bot-----------------------------------------
+
+'''--------- Responses to /Start CMD -----------'''
+@bot.message_handler(commands)
+def start_cmd(message):
+    bot.send_chat_action(message.chat.id, "Typing")
+    #Welcome Message
+    bot.send_message(message.chat.id, "Hola, me llamo Esfotito 🦉 y seré tu asistente para conocer sobre los procesos académicos en la ESFOT.")
+    bot.send_photo(message.chat.id, start_img, "<b>Bienvenido al Chatbot de la ESFOT - EPN</b>", parse_mode="html")
+
+'''--------- Respondes to /Matriculas ------------'''
+
+'''--------- Respondes to /Practicas ------------'''
+
+'''--------- Respondes to /Vinculacion ------------'''
+
+'''--------- Respondes to /Preguntas_frecuentes ------------'''
+
+'''--------- Respondes to /Comentarios ------------'''
+
+'''--------- Respondes to /Contactos ------------'''
+
+'''--------- Responses to Text ----------'''
 @bot.message_handler(content_types=["text"])
 def text_msgs(message):
-    bot.send_message(message.chat.id, "Un momento, te dirijo con Esfotito")
-        
-#Bot Main Statement
-if __name__ == '__main__':
-    print("Iniciando el Bot")
+    if message.text.startswith("/"):
+        bot.send_chat_action(message.chat.id, "Typing")
+        bot.send_message(message.chat.id, "Comando no disponible ‼" + "\n Intenta nuevamente con tu mensaje, por favor 👀")
+    else:
+        bot.send_chat_action(message.chat.id, "Typing")
+        edit_message = bot.send_message(message.chat.id, "Tienes muchas dudas y Esfotito te las solucionará 👋")
+        time.sleep(60)
+        bot.edit_message_text("Gracias por preguntarle a <i><u>Esfotito</u></i> 🦉", message.chat.id, edit_message.message_id, parse_mode="html")
+#----------------------------------------------------------------------
+
+# Main Funciton with infinity polling
+if __name__ == "__main__":
+    print(" ********************** Iniciando el Bot *********************** ")
     bot.infinity_polling()
-    print("Fin del Bot")
+    print(" ********************** Cerrando el Bot ************************ ")
